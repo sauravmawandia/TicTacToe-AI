68c2831a89dc9ce7c3a4f7863a6e451927dc2f9f
sauravmawandia <saurav.mawandia@gmail.com>
2017-04-18 19:48:31 -0400

    added copy of array to copy value of array rather than reference

--- src/ticTacToe/State.java
+++ src/ticTacToe/State.java
@@ -7,25 +7,30 @@ import java.util.ArrayList;
  * Created by saura on 4/15/2017.
  */
 public class State {
-    Player state[][];
+    private final Player state[][];
     private final int size;
     private final Player maxPlayer;
     private final Player minPlayer;
     private final ArrayList<Action> actions;
 
-    public State(int size, Player maxPlayer, Player minPlayer,Player[][] state) {
+    public State(int size, Player maxPlayer, Player minPlayer) {
         this.size = size;
-        this.state = state;
-        this.maxPlayer = maxPlayer;
-        actions = new ArrayList<>();
-        this.minPlayer=minPlayer;
-    }
-    public void initialize(){
+        this.state = new Player[size][size];
         for(int i=0;i<size;i++){
             for(int j=0;j<size;j++){
                 this.state[i][j]=Player.B;
             }
         }
+        this.maxPlayer = maxPlayer;
+        actions = new ArrayList<>();
+        this.minPlayer=minPlayer;
+    }
+    public State(int size, Player maxPlayer, Player minPlayer,Player[][] board) {
+        this.size = size;
+        this.state=board;
+        this.maxPlayer = maxPlayer;
+        actions = new ArrayList<>();
+        this.minPlayer=minPlayer;
     }
 
     public Player getMaxPlayer() {
--- src/ticTacToe/TicTacToe.java
+++ src/ticTacToe/TicTacToe.java
@@ -2,30 +2,31 @@ package ticTacToe;
 
 import java.io.*;
 import java.util.Scanner;
+import java.util.SplittableRandom;
 
 /**
  * Created by saura on 4/15/2017.
  */
 public class TicTacToe {
     public static void main(String[] args) throws IOException{
-        Player[][] state=new Player[3][3];
-        State s=new State(3,Player.X,Player.O,state);
-        s.initialize();
+        State s=new State(3,Player.X,Player.O);
         int count=0;
         AlphaBeta ab=new AlphaBeta();
         while(true){
-            System.out.println("Enter x and y of the position");
+            System.out.println("Enter x and y of the position"+s.getMaxPlayer());
             Scanner in = new Scanner(System.in);
             String input = in.nextLine();
             String[] ar=input.split(",");
             int x=Integer.parseInt(ar[0]);
             int y=Integer.parseInt(ar[1]);
+
             Action a=new Action(x,y);
             s.makeMove(a,s.getMaxPlayer());
             printWinner(s);
             printCurrentState(s.getState());
             System.out.println("Computer turn");
-            a=ab.alphaBetaSearch(s);
+            State newState=new State(3,Player.X,Player.O,copyArray(s.getState()));
+            a=ab.alphaBetaSearch(newState);
             s.makeMove(a,s.getMinPlayer());
             printCurrentState(s.getState());
             printWinner(s);
@@ -54,5 +55,14 @@ public class TicTacToe {
             }
             System.exit(0);
         }
+    }
+    private static Player[][] copyArray(Player[][] board){
+        Player a[][]=new Player[board.length][board[0].length];
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a.length;j++){
+                a[i][j]=board[i][j];
+            }
+        }
+        return a;
     }
 }
